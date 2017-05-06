ce8d06451a4c65a7ea660534dbdd87d844e29e42
Sage Han <zongshian@gmail.com>
2017-05-06 23:25:53 +0800

    new style for #cart

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,5 +1,5 @@
 <template>
-  <article id="cart">
+  <article id="cart" class="cart">
     <header class="cart__hd">
       <h1>购物车</h1>
     </header>
@@ -8,111 +8,125 @@
       <p>您还没有选择菜品哦!</p>
     </div>
     <!-- or -->
-    <form v-else class="cart__bd">
-      <table class="cart-items">
-        <caption>订购信息</caption>
+    <form v-else class="cart__bd steps">
 
-        <thead>
-          <tr>
-            <th>餐品</th><th>单价</th><th>数量</th><th>金额</th>
-          </tr>
-        </thead>
-
-        <tfoot>
-          <tr>
-            <td colspan="4">
-              <label for="comment">备注说明：</label>
-              <input class="comment-input" type="text" name="comment"  maxlength="15" placeholder="姐姐，麻麻说多放芝士更好吃！" />
-            </td>
-          </tr>
-
-          <tr class="coupon toggle--coupon">
-            <td colspan="3">
-              <label>优惠蜜语：</label>
-              <input class="coupon-input" type="text" name="coupon-code" placeholder="波罗菠萝蜜" lang="zh-cn" maxlength="15" />
-            </td>
-            <td class="CNY">-5</td>
-          </tr>
-
-          <tr>
-            <td colspan="3">外送费</td>
-            <td class="CNY">{{gratuity}}</td>
-          </tr>
-
-          <tr>
-            <td colspan="3">总额</td>
-            <td class="CNY">{{total}}</td>
-          </tr>
-        </tfoot>
-
-        <tbody>
-          <template v-for="i in cartItems" >
-            <tr class="h-item">
-              <td class="p-name">{{cuisineNameOf(i.productID)}}({{i.offers.name}})</td>
-              <td class="spec-price">{{specPriceOf(i.productID, i.offers.name)}}</td>
-              <td class="qty">{{i.qty}}</td>
-              <td class="sub-total CNY">{{specPriceOf(i.productID, i.offers.name) * i.qty}}</td>
+      <div class="step-wrap">
+        <table class="cart-items">
+          <caption class="step__hd">订购信息</caption>
+          <thead>
+            <tr>
+              <th>餐品</th><th>单价</th><th>数量</th><th>金额</th>
             </tr>
-          </template>
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            <template v-for="i in cartItems" >
+              <tr class="h-item">
+                <td class="p-name">{{cuisineNameOf(i.productID)}}({{i.offers.name}})</td>
+                <td class="spec-price">{{specPriceOf(i.productID, i.offers.name)}}</td>
+                <td class="qty">{{i.qty}}</td>
+                <td class="sub-total money">{{specPriceOf(i.productID, i.offers.name) * i.qty}}</td>
+              </tr>
+            </template>
+          </tbody>
+        </table>
 
-      <fieldset class="customer">
-        <legend>配送信息</legend>
-
-        <ol>
-          <li>
-            <label for="customer-name">称呼<span class="required">*</span></label>
-            <input :value="name" @input="updateCustomer" :class="{inValid: !nameValidity}" id="customer-name" type="text" name="customer-name" required maxlength="10"/>
-          </li>
-
-          <li>
-            <label for="customer-tel">手机<span class="required">*</span></label>
-            <input :value="telephone" @input="updateCustomer" :class="{inValid: !telValidity}" id="customer-tel" type="tel" name="customer-tel" required maxlength="11" />
-
-          </li>
-
-          <li>
-            <label for="customerCity">城市<span class="required">*</span></label>
-            <select id="customer-city" name="customer-city"><option value="乌鲁木齐" selected>乌鲁木齐市</option></select>
-          </li>
-
-          <li>
-            <label for="customer-addr">地址<span class="required">*</span></label>
-            <textarea :value="address" @input="updateCustomer" :class="{inValid: !addrValidity}" id="customer-addr" type="addr" name="customer-addr" required maxlength="50"></textarea>
-          </li>
-        </ol>
-
-        <fieldset class="reservation">
-          <input type="checkbox" id="is--reserved" name="is--reserved" />
-          <label for="is--reserved">预订</label>
-          <div class="reservation-option">
-            <select :value="delayday" @input="updateReservation" name="delayday">
-              <option v-for="o in delayDayOptions" v-bind:value="o.value">
-                {{o.text}}
-              </option>
-            </select>
-            <input :value="scheduledtime" @input="updateReservation" type="time" name="scheduledtime" placeholder="请输入时间" maxlength="8" />
-          </div>
+        <fieldset class="form-input line-item">
+            <label class="form-input__hd" for="comment">备注说明</label>
+            <input class="form-input__bd comment-input" type="text" name="comment"  maxlength="15" />
         </fieldset>
-      </fieldset>
 
-      <fieldset class="payment">
-        <legend>付款方式</legend>
-        <ol>
-          <li><input v-model="payment" value="Cash" type="radio" name="payment" id="cash" /><label for="cash">现金付</label></li>
-          <li><input v-model="payment" value="wx_pub" type="radio" name="payment" id="wechat" /><label for="wechat">微信支付</label></li>
-          <li><input v-model="payment" value="alipay_wap" type="radio" name="payment" id="alipay" /><label for="alipay">支付宝</label></li>
-        </ol>
-      </fieldset>
+        <div class="charge-info">
+          <fieldset class="form-input line-item">
+            <label class="form-input__hd">优惠蜜语</label>
+            <input class="form-input__bd coupon-input" type="text" name="coupon-code" maxlength="15" />
+          </fieldset>
 
-      <button
-        class="btn btn_submit"
-        name="place-order-button" 
-        :disabled="!(nameValidity && telValidity && addrValidity)" 
-        @click.prevent="checkout" >
-        提交订单
-      </button>
+          <div class="line-item">
+            <span>送餐费：</span>
+            <span class="money">{{gratuity}}</span>
+          </div>
+
+          <div class="cart-total line-item">
+            <span>总额：</span>
+            <span class="money">{{total}}</span>
+          </div>
+        </div>
+      </div>
+
+        <fieldset class="step-wrap customer">
+          <legend class="step__hd">配送信息</legend>
+
+          <ol class="step__bd">
+            <li class="form-input line-item">
+              <label class="form-input__hd"
+              for="customer-name">称呼<span class="asterisk">*</span></label>
+              <input class="form-input__bd" :value="name" @input="updateCustomer" :class="{inValid: !nameValidity}" id="customer-name" type="text" name="customer-name" required maxlength="10"/>
+            </li>
+
+            <li class="form-input line-item">
+              <label class="form-input__hd" for="customer-tel">手机<span class="asterisk">*</span></label>
+              <input class="form-input__bd" :value="telephone" @input="updateCustomer" :class="{inValid: !telValidity}" id="customer-tel" type="tel" name="customer-tel" required maxlength="11" />
+
+            </li>
+
+            <li class="form-input line-item">
+              <label class="form-input__hd" for="customerCity">城市<span class="asterisk">*</span></label>
+              <div class="form-input__bd">
+                <select id="customer-city" name="customer-city" >
+                  <option value="乌鲁木齐" selected>乌鲁木齐市</option>
+                </select>
+              </div>
+            </li>
+
+            <li class="form-input line-item">
+              <label class="form-input__hd" for="customer-addr">地址<span class="asterisk">*</span></label>
+              <textarea class="form-input__bd" :value="address" @input="updateCustomer" :class="{inValid: !addrValidity}" id="customer-addr" type="addr" name="customer-addr" required maxlength="50"></textarea>
+            </li>
+          </ol>
+        </fieldset>
+
+      <div class="step-wrap">
+        <fieldset class="payment">
+          <legend class="step__hd">付款方式</legend>
+
+          <fieldset class="reservation form-input line-item">
+            <input type="checkbox" id="is--reserved" name="is--reserved" />
+            <label class="form-input__hd" for="is--reserved">预订</label>
+            <fieldset class="form-input__bd reservation-options">
+              <select class="option__delayday" :value="delayday" @input="updateReservation" name="delayday">
+                <option v-for="o in delayDayOptions" v-bind:value="o.value">
+                  {{o.text}}
+                </option>
+              </select>
+              <input class="option__time" :value="scheduledtime" @input="updateReservation" type="time" name="scheduledtime" placeholder="请输入时间" maxlength="8" />
+            </fieldset>
+          </fieldset>
+
+          <ol class="step__bd">
+            <li class="payment-input">
+              <input v-model="payment" value="Cash" type="radio" name="payment" id="pay-by-cash" />
+              <label for="pay-by-cash">现金付</label>
+            </li>
+            <li class="payment-input">
+              <input v-model="payment" value="wx_pub" type="radio" name="payment" id="pay-by-wx" />
+              <label for="pay-by-wx">微信支付</label>
+            </li>
+            <li class="payment-input">
+              <input v-model="payment" value="alipay_wap" type="radio" name="payment" id="pay-by-alipay" />
+              <label for="pay-by-alipay">支付宝</label>
+            </li>
+          </ol>
+        </fieldset>
+
+        <input
+          class="submit-button"
+          name="place-order-button"
+          type="submit"
+          value="提交订单"
+          :disabled="!(nameValidity && telValidity && addrValidity)"
+          @click.prevent="checkout" >
+        </input>
+      </div>
   </form>
 </article>
 </template>
